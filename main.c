91ef0cf030fc176204ce9b5a0d535a76f93738dc
Wronka-99 <62218451+Wronka-99@users.noreply.github.com>
2022-01-08 01:05:55 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,140 @@
  #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+#define SIZE_MD5 35
+#define L_HASEL_MD5 420
+#define L_SLOW_SLOWNIK 550
+#define DL_SLOWA 32
+
+
+char *wczytaj(char *z, int ile, FILE *plik);
+
+ int main(int argc, char *argv[])
+ {
+
+     //############## ZMIENNE ############## 
+
+     FILE *slownik;
+     FILE *hasla_MD5;
+
+     char tab_hasla_MD5[L_HASEL_MD5][SIZE_MD5];
+     char* tab_slownik[L_SLOW_SLOWNIK];
+     char tab_pom[DL_SLOWA];
+     char tab_pom2[SIZE_MD5];
+     char *tmp_wsk;
+     char *tmp_wsk2;
+
+
+    //######################################
+
+    //############## SPRAWDZENIE LICZBY ARGUMENTOW ############## 
+     if(argc!=3)
+     {
+         puts("Podano niewlasciwa liczbe argumentow.");
+         puts("Poprawna liczba to dwa argumenty.");
+     }
+
+    //############## PROBA OTWARCIA PLIKOW SLOWNIK I KODY MD5
+     if((slownik=fopen(argv[1],"r"))==NULL)
+     {
+        printf("Nie mozna otworzyc pliku %s\n",argv[1]);
+        exit(EXIT_FAILURE);
+     }
+
+     if((hasla_MD5=fopen(argv[2],"r"))==NULL)
+     {
+        printf("Nie mozna otworzyc pliku %s\n",argv[2]);
+        exit(EXIT_FAILURE);
+     }
+
+
+ 
+
+      for (int index = 0; index < L_HASEL_MD5; index++) 
+    {
+        tmp_wsk=fgets (tab_hasla_MD5[index], SIZE_MD5, hasla_MD5);   // czytamy ze standardowego wejścia
+        
+        //printf("Dlugos kodu nr. %d ze md5 %ld \n",index, strlen(tab_hasla_MD5[index]));
+
+        if (tmp_wsk != NULL) 
+        {      
+            if (feof (hasla_MD5))
+                printf ("\n%d koniec pliku\n", index);
+        }
+        else
+            printf ("\n%d blad odczytu\n", index);
+    }
+
+
+      for (int index = 0; index < L_SLOW_SLOWNIK ; index++) 
+    {
+        tmp_wsk=fgets (tab_pom,DL_SLOWA,slownik);   // czytamy ze standardowego wejścia
+        tab_slownik[index]=(char *)calloc(strlen(tab_pom),sizeof(char));
+
+        wczytaj(tab_pom,DL_SLOWA,slownik);
+        strncpy(tab_slownik[index],tab_pom,strlen(tab_pom));
+        
+
+        if (tmp_wsk != NULL) 
+        {      
+            if (feof (slownik))
+                printf ("\n%d koniec pliku\n", index);
+        }
+        else
+            printf ("\n%d blad odczytu\n", index);
+    }
+
+    for(int index=0; index<L_HASEL_MD5; index++)
+    {
+        printf("Kod nr:%d wyglada nastepujaco: %s ",index,tab_hasla_MD5[index]);
+    }
+        
+    printf("Slowo ze slownika %s\n", tab_slownik[450]);
+
+    fclose(slownik);
+    fclose(hasla_MD5);
+
+    for(int i=0; i<L_SLOW_SLOWNIK;i++)
+    {
+       free(tab_slownik[i]); 
+    }
+    
+    
+
+    return 0;
+ }
+ 
+
+
+
+
+
+
+ char *wczytaj(char *z, int ile, FILE *plik)
+ {
+     char *wynik;
+     int i=0;
+
+     wynik=fgets(z,ile, plik);
+
+     if(wynik)
+     {
+         while(z[i]!= '\n' && z[i]!= '\0')
+            i++;
+        if(z[i]=='\n')
+            z[i]='\0';
+        else
+            while(getchar()!='\n')
+                continue;
+     }
+ }
+
+ 
+ 
+ 
+ 
+ /*#include <stdio.h>
  #include <string.h>
  #include <openssl/evp.h>
 
@@ -41,3 +177,5 @@
 
      exit(0);
  }
+
+ */
